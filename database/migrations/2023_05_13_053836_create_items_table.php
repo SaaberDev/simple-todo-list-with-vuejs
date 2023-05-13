23ce5d896a4cf2c212e5d3fdfb779093812a3455
SaaberDev <saaberdev@gmail.com>
2023-05-13 08:07:47 +0200

    - added migration, model, controller

---
+++ database/migrations/2023_05_13_053836_create_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index()->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->tinyInteger('position');
+            $table->tinyInteger('completed_at')->comment('(0) = Incomplete, (1) = Complete ');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
