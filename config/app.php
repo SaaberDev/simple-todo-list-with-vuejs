3c8ae18f165a824ec7ffb74126e33aa9a7b4dcbc
SaaberDev <saaberdev@gmail.com>
2023-05-18 22:37:04 +0200

    - updated README with installation guide

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://todo-test.test'),
+    'url' => env('APP_URL', 'http://localhost:8000'),
     'api_url' => env('API_URL', '/api/v1'),
 
     'asset_url' => env('ASSET_URL'),
